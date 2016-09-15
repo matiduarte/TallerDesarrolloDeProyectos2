a7ae07ddbedd841f3daaf60398a725801bcccdbf
Matias Carreras <mncarreras@hotmail.com>
2016-09-15 03:54:11 -0300

    implementacion del login con facebook #2

--- Cliente/TallerDeProyectos2/app/src/main/java/fiuba/tallerdeproyectos2/Activities/LoginActivity.java
+++ Cliente/TallerDeProyectos2/app/src/main/java/fiuba/tallerdeproyectos2/Activities/LoginActivity.java
@@ -5,21 +5,56 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import fiuba.tallerdeproyectos2.R;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+import java.util.Arrays;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_login);
 
-        Button facebookButton = (Button) findViewById(R.id.facebook_button);
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Intent homeIntent = new Intent(getApplicationContext(), MainActivity.class);
+                homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                homeIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(homeIntent);
+                finish();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(), R.string.facebook_cancel_login, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Button facebookButton = (Button)findViewById(R.id.facebook_button);
         facebookButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                facebookLogin();
+                LoginManager.getInstance().logInWithReadPermissions(LoginActivity.this, Arrays.asList("public_profile", "email"));
             }
         });
 
@@ -32,12 +67,10 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    private void facebookLogin(){
-        Intent homeIntent = new Intent(this,MainActivity.class);
-        homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        homeIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(homeIntent);
-        finish();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     private void googleLogin(){
