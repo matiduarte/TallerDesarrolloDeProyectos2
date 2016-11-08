f32a2322769d7879164dbed60a76a3be48026c7b
quimeyfunes <quimey.funes@gmail.com>
2016-11-08 20:33:21 -0300

    agrego servicio para los certificados

---
+++ Servidor/src/entities/Certification.java
@@ -0,0 +1,31 @@
+package entities;
+
+import java.util.List;
+
+import dataBase.StoreData;
+
+public class Certification {
+	private String studentName;
+	private String courseName;
+	private float result;
+	
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getCourseName() {
+		return courseName;
+	}
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+	public float getResult() {
+		return result;
+	}
+	public void setResult(float result) {
+		this.result = result;
+	}
+	
+}
